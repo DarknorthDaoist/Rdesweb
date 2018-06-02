74bf7f804826d6c6b61e4062e52bc651bef98aeb
madkileras <arthur.pena@usach.cl>
2018-06-02 18:49:42 -0400

    se sube todo, con intento de busqueda por fecha

--- Back/src/main/java/cl/redesUsach/redes/services/SignalService.java
+++ Back/src/main/java/cl/redesUsach/redes/services/SignalService.java
@@ -3,9 +3,17 @@ package cl.redesUsach.redes.services;
 
 import cl.redesUsach.redes.models.Signal;
 import cl.redesUsach.redes.repositories.SignalRepository;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @CrossOrigin
 @RestController
@@ -14,11 +22,26 @@ public class SignalService {
     @Autowired
     SignalRepository signalRepository;
 
+
+
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public Iterable<Signal> getAllSignals(){
         return signalRepository.findAll();
     }
+/*
+    @RequestMapping(value= "/fecha/{fecha}",method = RequestMethod.GET)
+    @ResponseBody
+    public String getSignalsByDate(@PathVariable("fecha") String fecha ){
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyy HH:mm");
+        try{
+            Date temp=formatter.parse(fecha);
+            return signalRepository.findAllByFecha(temp).toString();
+        }catch(ParseException e){
+            return "Error "+fecha;
+        }
+
+    }*/
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
